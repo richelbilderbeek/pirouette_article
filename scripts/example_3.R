6f6605606b8885726463bce86f83adac1ff2b132
richelbilderbeek <richel@richelbilderbeek.nl>
2019-02-21 13:55:50 +0100

    Add experiment setup, progress https://github.com/richelbilderbeek/pirouette/issues/58

--- scripts/example_3.R
+++ scripts/example_3.R
@@ -1,20 +1,25 @@
-# Code of example 2
+# Code of example 3
 #
-# Works under Linux and MacOS only
+# Works under Windows
 library(pirouette)
 
-phylogeny  <- ape::read.tree(text = "((A:4, B:4):1, (C:4, D:4) :1);")
+#phylogeny <- create_ideal_tree(n_taxa = 5, crown_age = 10)
+# STUB
+phylogeny <- create_stunning_tree(n_taxa = 5, t_0 = 10)
 
 alignment_params <- create_alignment_params(
   root_sequence = create_blocked_dna(length = 1000),
   mutation_rate = 0.1
 )
 
-experiments <- create_all_experiments()
+experiments <- list(create_experiment())
+
+twinning_params <- create_twinning_params()
 
 pir_params <- create_pir_params(
   alignment_params = alignment_params,
-  experiments = experiments
+  experiments = experiments,
+  twinning_params = twinning_params
 )
 
 errors <- pir_run(
@@ -24,19 +29,8 @@ errors <- pir_run(
 
 
 library(ggplot2)
-
-pir_plot(errors) + theme(
-  panel.background = element_rect(
-    fill = "white",
-    colour = "black"
-  ),
-  panel.grid.major = element_line(colour = "black"),
-  panel.grid.minor = element_line(color = "black")
-
-)
-
 library(ggthemes)
-pir_plot(errors) + theme_wsj() + ggsave("/home/richel/GitHubs/pirouette_article/figure_3.png")
+pir_plot(errors) + theme_wsj() + ggsave("/home/richel/GitHubs/pirouette_article/figure_4.png")
 
 pir_plot(errors) + theme_tufte()
 pir_plot(errors) + theme_economist()
