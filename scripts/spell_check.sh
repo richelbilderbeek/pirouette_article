d92e587055b5d8c25866789a37957932db89abe7
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-25 14:34:21 +0100

    Spellcheck

--- scripts/spell_check.sh
+++ scripts/spell_check.sh
@@ -6,4 +6,5 @@
 #
 
 Rscript -e 'spelling::spell_check_files("content.tex", ignore = readLines("inst/WORDLIST"))'
+Rscript -e 'spelling::spell_check_files("response_letter/README.md", ignore = readLines("inst/WORDLIST"))'
 
