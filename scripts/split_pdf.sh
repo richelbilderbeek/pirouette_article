13f3d41f4ae0baa2538d26cde8d0fda154270a80
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-31 07:35:13 +0200

    Supplementary materials also have references

--- scripts/split_pdf.sh
+++ scripts/split_pdf.sh
@@ -13,5 +13,5 @@
 #   sudo apt-get install pdftk
 #
 
-pdftk article.pdf cat 1-35 output main.pdf
-pdftk article.pdf cat 36-45 output supp_mat.pdf
+pdftk pirouette_article.pdf cat 1-24 output main.pdf
+pdftk pirouette_article.pdf cat 25-end output supp_mat.pdf
