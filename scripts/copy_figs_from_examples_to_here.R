5b5757a09e1bc2a3f4a048d1cc6a7eac1f53d8bc
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-28 17:36:55 +0200

    Also copy 'phylogeny.newick'

--- scripts/copy_figs_from_examples_to_here.R
+++ scripts/copy_figs_from_examples_to_here.R
@@ -22,7 +22,7 @@ png_and_latex_files <- list.files(
 # Only pirouette examples
 files <- stringr::str_subset(
   string = png_and_latex_files,
-  pattern = ".*pirouette_example_.*(likelihoods|true|twin|errors|esses).*$"
+  pattern = ".*pirouette_example_.*(phylogeny|likelihoods|true|twin|errors|esses).*$"
 )
 
 files <- stringr::str_subset(
