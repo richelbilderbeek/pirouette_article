b48150fb0f573da45832a08ce020030602cb673f
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-01 13:57:53 +0200

    Scripts are executables

--- scripts/split_pdf.sh
+++ scripts/split_pdf.sh
@@ -13,5 +13,5 @@
 #   sudo apt-get install pdftk
 #
 
-pdftk pirouette_article.pdf cat 1-24 output main.pdf
-pdftk pirouette_article.pdf cat 25-end output supp_mat.pdf
+pdftk pirouette_article.pdf cat 1-25 output main.pdf
+pdftk pirouette_article.pdf cat 26-end output supp_mat.pdf
